ca28845353b3625ef864db39568e9ab6846ddee8
xunxxoie <ahh0520@naver.com>
2024-12-28 14:31:41 +0900

    [Feat] Initial Commit

---
+++ src/test/java/org/inffy/InffyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InffyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
