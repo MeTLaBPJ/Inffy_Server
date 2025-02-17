2bc5e265f34dc031b1369ed70211647a8dd5b39d
d6nggyun <d6nggyun@gmail.com>
2025-02-17 19:39:00 +0900

    [Refactor] CORS Server Url

--- src/main/java/org/inffy/global/config/SwaggerConfig.java
+++ src/main/java/org/inffy/global/config/SwaggerConfig.java
@@ -5,28 +5,33 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Info info = new Info()
+                .title("Inffy Server")
+                .description("인천대학교 소모임 주선 서비스 'Inffy' 서버")
+                .version("1.0.0");
+
+        Server server = new Server();
+        server.setUrl("https://inffy-server.site");
+
         return new OpenAPI()
-                .info(apiInfo())
+                .info(info)
+                .servers(List.of(server))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
-    }
-
-    private Info apiInfo() {
-        return new Info()
-                .title("Inffy Server")
-                .description("인천대학교 소모임 주선 서비스 'Inffy' 서버")
-                .version("1.0.0");
     }
 }
